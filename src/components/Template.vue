3ea714c6127c1ee2c4b47b47c783e566c1a763a6
Dan <dlaubacher3@gmail.com>
2018-10-29 00:27:18 -0400

    initial vue commit + example file

---
+++ src/components/Template.vue
@@ -0,0 +1,42 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Example",
+  props: {
+    example: {
+      type: String,
+      default: "all"
+    },
+  },
+  components: {},
+  data() {
+    return {
+    };
+  },
+  computed: {
+    ...mapState([]),
+    ...mapGetters([]),
+    example() {}
+  },
+  methods: {
+    example() {
+    },
+    ...mapActions([])
+  },
+  watch: {
+    propName: function() {},
+  },
+  mounted() {}
+};
+</script>
+
+<style scoped>
+
+</style>
