733e43f3637d00be2b41139e1441ebf78c7c3465
Dan <dlaubacher3@gmail.com>
2018-11-05 18:58:09 -0500

    add messages modal

---
+++ src/components/ModalMessage.vue
@@ -0,0 +1,74 @@
+<template>
+<transition name="modal">
+   <div v-if="active">
+     <div class="modal opacity"></div>
+      <div class="modal">
+         <div class="card">
+          <div class="content">
+              {{message}}
+              <button @click="$emit('closeModal')">close</button>
+          </div>
+        </div>
+      </div>
+  </div>
+</transition>
+</template>
+
+<script>
+export default {
+  name: 'ModalMessage',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    message: {
+      type: String,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+}
+
+.opacity {
+  background: #121515;
+  opacity: 0.9;
+}
+
+.card {
+  background: white;
+  padding: 2em;
+}
+
+.content {
+  font-size: 1.2em;
+  color: black;
+}
+.modal-enter-active {
+  z-index: 10;
+  transition: all 0.5s;
+}
+
+.modal-leave-active {
+  transition: all 0.5s;
+}
+
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+}
+</style>
